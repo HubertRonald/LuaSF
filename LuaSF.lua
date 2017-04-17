3f7cd746d15ac5c7a2d8cb881bc45d1c8572dbdd
HubertRonald <hubert.ronald@gmail.com>
2017-04-17 15:52:29 -0500

    Fix some bugs, add Random Variables Lognormal
    Add math.pi, math.exp and math.pow

--- LuaSF.lua
+++ LuaSF.lua
@@ -10,7 +10,11 @@ return {
 	
 -- math functions
 rand = math.random,
-ln = math.log,
+ln = math.log,		-- natural logarithm
+sqrt = math.sqrt,
+pi = math.pi,
+exp = math.exp,		-- e exponent	
+pow = math.pow,	
 
 -- sum array function
 sumF = function (array)
@@ -21,14 +25,14 @@ end,
 
 -- average array function
 avF = function (array)
-	local st = requiere(dirLuaStat) -- auto reference
+	local st = require(dirLuaStat) -- auto reference
 	local s = st.sumF(array)
 	return s/#array
 end,
 
 -- (n-1) standar desviation
 stvF = function(array)
-	local st =requiere(dirLuaStat) -- auto reference
+	local st =require(dirLuaStat) -- auto reference
 	local xp, sq = st.avF(array), 0
 	for _,v in pairs(array) do sq = sq + (v - xp)^2 end
 	return (sq/(#array-1))^0.5
@@ -61,7 +65,7 @@ end,
 normal_inv_D = function (p, mu, sig)
 	-- nomal standar mu=0,sig=1, p~[0,1]: (probability)
 	-- method schmeiser
-	local mu, sig = mu or 0, sig or 1
+	local mu, sig, p = mu or 0, sig or 1, 0.5
 	local z = (p^0.135 - (1-p)^0.135)/0.1975
 	return z*sig + mu
 end,
@@ -73,6 +77,7 @@ weibullD = function (alpha,beta) local st = require(dirLuaStat); return alpha*(-
 
 trianD = function(a,b,c)
 	local st = require(dirLuaStat)	-- auto reference
+	local a,b,c = a or 1, b or 2, c or 3
 	if st.rand() <= (b-a)/(c-1) then
 		return a + ((b-a)*(c-a)*r)^0.5
 	else
@@ -81,16 +86,16 @@ trianD = function(a,b,c)
 end,
 
 binomialD = function(n,p)
-	local st = requiere(dirLuaStat) -- auto reference
-	local va = 0
+	local st = require(dirLuaStat) -- auto reference
+	local n, p , va = n or 1, p or 0.5, 0
 	-- convolution method
 	for i=1,n do va=va+st.bernoulliD(p) end
 	return va
 end,
 
 poissonD = function(lamba)
-	local st = requiere(dirLuaStat) -- auto reference
-	local t, va = 0, 0
+	local st = require(dirLuaStat) -- auto reference
+	local t, va, lamba = 0, 0, lamba or 0.5
 	-- convolution method
 	while true do
 		t = t+st.expoD(1/lamba)
@@ -100,7 +105,7 @@ poissonD = function(lamba)
 end,
 
 chiSquare = function(n)
-	local st = requiere(dirLuaStat) -- auto reference
+	local st = require(dirLuaStat) -- auto reference
 	local va = 0
 	for i=1, n do va = va + st.normalD() end
 	return va^0.5
@@ -117,11 +122,12 @@ gamRand = function(alpha, lamba)
 		------------------------------------------------------------
 	]]
 	local st = requiere(dirLuaStat) -- auto reference
+	local alpha, lamba = alpha or 0.5, lamba or 0.5
 	local va=0
 	if alpha >= 1 then
 		local d=alpha-1/3
 		local c=1/(9*d)^.5
-		while true
+		while (true) do
 			local Z=st.normalD()
 			if Z>-1/c then
 				local V=(1+c*Z)^3
@@ -140,4 +146,22 @@ gamRand = function(alpha, lamba)
 	return va
 end,
 
+lognoRand = function(m, s)
+	--[[
+		------------------------------------------------------------
+		-- Took
+		-- See details in these links
+		-- http://stackoverflow.com/questions/23699738/how-to-create-a-random-number-following-a-lognormal-distribution-in-excel
+		-- http://blogs.sas.com/content/iml/2014/06/04/simulate-lognormal-data-with-specified-mean-and-variance.html
+		------------------------------------------------------------
+	]]
+	local st = require(dirLuaStat) -- auto reference
+	local m, s = m or 0, s or 1
+	-- Next step is to scale the mean and standard deviation
+	local mean = st.ln( m^2 / st.sqrt( m^2 + s^2 ))
+	local sd = st.sqrt( st.ln(( m^2 + s^2 ) / m^2 ))
+	
+	local x = st.normal_inv_D(st.rand, mean, sd)
+	return st.exp(x)
+end,
 }
